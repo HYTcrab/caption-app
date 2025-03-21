d52647747946aa4a1bdb2dd3646db403a49f661c
cydary <hytcrab@outlook.com>
2025-03-22 02:04:40 +0800

    modify

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          // 将React相关库打包到一个文件中
+          'react-vendor': ['react', 'react-dom'],
+          // 将Ant Design相关库打包到一个文件中
+          'antd-vendor': ['antd'],
+        }
+      }
+    },
+    // 调整警告限制（可选）
+    chunkSizeWarningLimit: 800,
+  }
 })
